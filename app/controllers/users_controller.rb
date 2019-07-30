46767d034d3436a26669012de70fb3052b59b387
softmax0112 <softmax0112@gmail.com>
2019-07-31 00:15:48 +0500

    defined actions for change password and edit for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,22 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :shorten_params, only: %i[update update_password]
-  before_action :set_user, only: %i[show edit update destroy]
+  before_action :set_user, only: %i[show edit update destroy change_password update_password]
 
   def show; end
 
   def edit; end
+
+  def change_password; end
+
+  def update_password
+    if current_user.valid_password?(@user_params[:password])
+      validate_and_confirm_password_update
+    else
+      @user.errors.add(:password, 'Current password does not match')
+      render :change_password
+    end
+  end
 
   def update
     if @user.update(user_params)
@@ -34,5 +45,25 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :name, :password, :image)
+  end
+
+  def confirm_password_update
+    if @user_params[:new_password] == @user_params[:confirm_password]
+      @user.update(password: @user_params[:new_password])
+      sign_in(@user, bypass: true)
+      redirect_to home_path, notice: 'Password successfully changed'
+    else
+      @user.errors.add(:new_password, 'Password confirmations do not match')
+      render :change_password
+    end
+  end
+
+  def validate_and_confirm_password_update
+    if @user_params[:new_password].present?
+      confirm_password_update
+    else
+      @user.errors.add(:new_password, 'Please enter a new password to update')
+      render :change_password
+    end
   end
 end
