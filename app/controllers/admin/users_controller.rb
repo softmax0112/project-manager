cd449a0746d098b31046fc18919e07ff332bea11
softmax0112 <softmax0112@gmail.com>
2019-07-31 00:56:18 +0500

    Added admin/users edit/update functionality

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,6 +4,16 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: %i[edit update toggle destroy]
 
+  def edit; end
+
+  def update
+    if @user.update(user_permit_params)
+      redirect_to home_path, notice: 'User succesfully updated'
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @users = if params[:name]
                User.where('name LIKE ?', "%#{params[:name]}%").page(params[:page])
