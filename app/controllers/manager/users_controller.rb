d2690b6b36a5c2b4806e98fc637ffde78f5ae2ed
softmax0112 <softmax0112@gmail.com>
2019-07-31 01:16:24 +0500

    Added callbacks and user_params in manager/users

--- app/controllers/manager/users_controller.rb
+++ app/controllers/manager/users_controller.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
+
 class Manager::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def set_user
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :image)
+  end
 end
