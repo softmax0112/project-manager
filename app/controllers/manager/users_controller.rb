3a50617af7428a124bd365a0d9f6f7c514fe35b4
softmax0112 <softmax0112@gmail.com>
2019-07-31 01:21:34 +0500

    Added manager show action + view

--- app/controllers/manager/users_controller.rb
+++ app/controllers/manager/users_controller.rb
@@ -4,6 +4,14 @@ class Manager::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: %i[show edit update destroy]
 
+  def show
+    #@clients = if params[:name]
+    #             Client.where('name LIKE ?', "%#{params[:name]}%").page(params[:page])
+    #           else
+    #             Client.page(params[:page])
+    #           end
+  end
+
   def set_user
     @user = User.find(params[:id])
     authorize @user
