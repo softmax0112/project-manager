106b10cd3a9d3d27f2f9e75993d18191b7b75102
softmax0112 <softmax0112@gmail.com>
2019-07-30 22:45:59 +0500

    Added home path helper method in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def home_path
+    return admin_user_path(current_user) if current_user.admin?
+    return user_path(current_user) if current_user.user?
+    return manager_user_path(current_user) if current_user.manager?
+  end
 end
