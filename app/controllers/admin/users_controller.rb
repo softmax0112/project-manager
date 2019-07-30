76f73fd46b71f63b8dc92e5d1918ea7b5d588b63
softmax0112 <softmax0112@gmail.com>
2019-07-31 00:52:54 +0500

    Added callbacks and permit_params in admin/users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: %i[edit update toggle destroy]
+
+  def set_user
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :role, :image)
+  end
 end
