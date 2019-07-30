cdd12d952358b6be250712b810116e1047b93755
softmax0112 <softmax0112@gmail.com>
2019-07-31 00:54:22 +0500

    Defined admin/users#show action+view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,6 +4,14 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: %i[edit update toggle destroy]
 
+  def show
+    @users = if params[:name]
+               User.where('name LIKE ?', "%#{params[:name]}%").page(params[:page])
+             else
+               User.page(params[:page])
+             end
+  end
+
   def set_user
     @user = User.find(params[:id])
     authorize @user
