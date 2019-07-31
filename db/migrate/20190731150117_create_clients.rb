682a239abc0b91dba668c0b2e13c8424d8172089
softmax0112 <softmax0112@gmail.com>
2019-07-31 20:05:39 +0500

    Created new clients table and model

---
+++ db/migrate/20190731150117_create_clients.rb
@@ -0,0 +1,8 @@
+class CreateClients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :clients do |t|
+      t.string :name, null: false, index: true
+      t.string :affiliation
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_203809) do
+ActiveRecord::Schema.define(version: 2019_07_31_150117) do
 
   create_table "clients", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "affiliation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_clients_on_name"
   end
 
   create_table "users", force: :cascade do |t|
