86ae904774c8ba75730ab5b4873651bad92b6894
softmax0112 <softmax0112@gmail.com>
2019-08-01 00:38:38 +0500

    Client validations added

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Client < ApplicationRecord
+  validates :name, presence: true,
+                   length: { minimum: 1, maximum: 50 },
+                   format: { with: /[a-zA-Z\s]*/, message: 'Only letters allowed' }
 end
