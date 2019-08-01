8798529a7769f24378847959f69eab22908aed05
softmax0112 <softmax0112@gmail.com>
2019-08-01 10:57:44 +0500

    Added client validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,4 +4,6 @@ class Client < ApplicationRecord
   validates :name, presence: true,
                    length: { minimum: 1, maximum: 50 },
                    format: { with: /[a-zA-Z\s]*/, message: 'Only letters allowed' }
+
+  validates :affiliation, length: { maximum: 50 }
 end
