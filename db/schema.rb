1f8a2c25ffc0b75aa72d83f2017b4f3d2a1b0292
softmax0112 <softmax0112@gmail.com>
2019-08-01 12:47:27 +0500

    Changed schema structure file

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,41 +12,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_063850) do
-
-  create_table "clients", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "affiliation"
-    t.index ["name"], name: "index_clients_on_name"
+ActiveRecord::Schema.define(version: 20_190_801_063_850) do
+  create_table 'clients', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'affiliation'
+    t.index ['name'], name: 'index_clients_on_name'
   end
 
-  create_table "projects", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.integer "hours_spent"
-    t.integer "total_payment"
-    t.integer "manager_id"
-    t.integer "creator_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["creator_id"], name: "index_projects_on_creator_id"
-    t.index ["manager_id"], name: "index_projects_on_manager_id"
+  create_table 'projects', force: :cascade do |t|
+    t.string 'title'
+    t.string 'description'
+    t.integer 'hours_spent'
+    t.integer 'total_payment'
+    t.integer 'manager_id'
+    t.integer 'creator_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['creator_id'], name: 'index_projects_on_creator_id'
+    t.index ['manager_id'], name: 'index_projects_on_manager_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name", null: false
-    t.boolean "enabled", default: true
-    t.string "role", default: "user"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "image"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'name', null: false
+    t.boolean 'enabled', default: true
+    t.string 'role', default: 'user'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'image'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
